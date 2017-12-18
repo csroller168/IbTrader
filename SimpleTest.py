1d017783caa8c1b377f4ace7c3bd3474262226d8
csroller168 <chrisshort168@gmail.com>
2017-12-18 15:28:44 -0500

    get rid of sample time stuff

--- SimpleTest.py
+++ SimpleTest.py
@@ -1,5 +1,4 @@
 # TODOs:
-# change getting time to IBApi.EWrapper.nextValidId
 # refactor into separate classes
 
 from ibapi.wrapper import EWrapper
@@ -8,10 +7,6 @@ from threading import Thread
 import queue
 
 class TestWrapper(EWrapper):
-    """
-    The wrapper deals with the action coming back from the IB gateway or TWS instance
-    We override methods in EWrapper that will get called when this action happens, like currentTime
-    """
 
     ## error handling code
     def init_error(self):
@@ -27,7 +22,6 @@ class TestWrapper(EWrapper):
 
         return None
 
-
     def is_error(self):
         an_error_if=not self._my_errors.empty()
         return an_error_if
@@ -37,21 +31,10 @@ class TestWrapper(EWrapper):
         errormsg = "IB error id %d errorcode %d string %s" % (id, errorCode, errorString)
         self._my_errors.put(errormsg)
 
-    ## Time telling code
-    def init_time(self):
-        time_queue=queue.Queue()
-        self._time_queue = time_queue
-
-        return time_queue
-
     def init_valid_id(self):
         id_queue = queue.Queue()
         self._id_queue = id_queue
         return id_queue
-
-    def currentTime(self, time_from_server):
-        ## Overriden method
-        self._time_queue.put(time_from_server)
 
     def nextValidId(self, id_from_server):
         self._id_queue.put(id_from_server)
@@ -68,41 +51,12 @@ class TestClient(EClient):
         ## Set up with a wrapper inside
         EClient.__init__(self, wrapper)
 
-    def speaking_clock(self):
-        """
-        Basic example to tell the time
-        :return: unix time, as an int
-        """
-
-        print("Getting the time from the server... ")
-
-        ## Make a place to store the time we're going to return
-        ## This is a queue
-        time_storage=self.wrapper.init_time()
-
-        ## This is the native method in EClient, asks the server to send us the time please
-        self.reqCurrentTime()
-
-        ## Try and get a valid time
-        MAX_WAIT_SECONDS = 10
-
-        try:
-            current_time = time_storage.get(timeout=MAX_WAIT_SECONDS)
-        except queue.Empty:
-            print("Exceeded maximum wait for wrapper to respond")
-            current_time = None
-
-        while self.wrapper.is_error():
-            print(self.get_error())
-
-        return current_time
-
     def next_valid_id(self):
         id_store = self.wrapper.init_valid_id()
         print("Getting next valid ID from the server... ")
         self.reqIds(1)
 
-        ## Try and get a valid time
+        ## Try and get next valid ID
         MAX_WAIT_SECONDS = 10
 
         try:
