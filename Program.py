664107bbec3edb963d3e557f9793b9bc90f5fc6a
csroller168 <chrisshort168@gmail.com>
2018-02-18 14:03:30 -0500

    take the action to perform as command line option

--- Program.py
+++ Program.py
@@ -1,29 +1,25 @@
-from IbRepo import IbRepo
-from time import sleep
-from SectorRotationStrategy import SectorRotationStrategy
-from OrderGenerator import OrderGenerator
+import sys
 from datetime import date
 from Backtester import Backtester
-from datetime import date
-
+from IbRepo import IbRepo
+from OrderGenerator import OrderGenerator
+from SectorRotationStrategy import SectorRotationStrategy
 
 if __name__ == '__main__':
-    #Backtester(100000, date(2005, 1, 3), date(2018, 1, 20)).Run()
 
-    # todo: maybe make this take an argument that signals what action to perform
+    opt = sys.argv[1]
 
-    # todo: add cash via reqAccountSummary (http://interactivebrokers.github.io/tws-api/classIBApi_1_1EClient.html#a3e0d55d36cd416639b97ee6e47a86fe9)
-    # todo: request contracts rather than build them
-
-
-    app = IbRepo("127.0.0.1", 4002, 168)
-    currentPortfolio = app.get_current_portfolio()
-    cashValue = app.get_cash_value()
-    value = float(sum(a.Value for a in currentPortfolio))
-    value += cashValue
-    targetPortfolio = SectorRotationStrategy(value, date.today()).GetTargetPortfolio()
-    orders = OrderGenerator().MakeOrders(currentPortfolio, targetPortfolio)
-    for order in orders:
-        app.orderStock(order._type, order._asset._numShares, order._asset._symbol)
-
-    app.disconnect()
+    if(opt == '1'):
+        Backtester(100000, date(2005, 1, 3), date(2018, 1, 20)).Run()
+    elif(opt == '2'):
+        # todo: request contracts rather than build them
+        app = IbRepo("127.0.0.1", 4002, 168)
+        currentPortfolio = app.get_current_portfolio()
+        cashValue = app.get_cash_value()
+        value = float(sum(a.Value for a in currentPortfolio))
+        value += cashValue
+        targetPortfolio = SectorRotationStrategy(value, date.today()).GetTargetPortfolio()
+        orders = OrderGenerator().MakeOrders(currentPortfolio, targetPortfolio)
+        for order in orders:
+            app.orderStock(order._type, order._asset._numShares, order._asset._symbol)
+        app.disconnect()
