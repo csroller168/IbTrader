798b4fbd293bc62fa6b36157beb4c512a4728aec
Ubuntu <ubuntu@ip-172-31-44-235.us-east-2.compute.internal>
2018-04-16 12:38:19 -0400

    add scripts

---
+++ scripts/rebalance.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+PATH=/home/ubuntu/bin:/home/ubuntu/.local/bin:/home/ubuntu/miniconda3/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+
+python3 /home/ubuntu/Git/IbTrader/Program.py 2 &> /home/ubuntu/Git/IbTrader/log/out.txt
+
