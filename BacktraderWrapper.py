29c3de14ef2ae6ff70c41e5b46d6682acc988cf7
csroller168 <chrisshort168@gmail.com>
2018-05-08 23:02:56 -0400

    Working example with PandasRepo integration

--- BacktraderWrapper.py
+++ BacktraderWrapper.py
@@ -1,13 +1,40 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 import backtrader as bt
+from datetime import datetime
+from FirstBacktraderStrategy import FirstBacktraderStrategy
+from PandasRepo import PandasRepo
+
+# todo:
+# 1. Make the repo return a dataframe within the start/end date
+# 2. make PandasRepo return df that backtrader can handle as data
+    # From example, re-index so that only date is the index
 
 class BacktraderWrapper:
     def __init__(self):
         pass
 
     def RunBackTest(self):
+        # Variable for our starting cash
+        startcash = 10000
+
+        # Create an instance of cerebro
         cerebro = bt.Cerebro()
-        print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
-        cerebro.run()
+
+        # Add our strategy
+        cerebro.addstrategy(FirstBacktraderStrategy)
+
+        # Add data feed
+        df = PandasRepo().GetData('IYF', datetime(2016, 1, 1), datetime(2017, 1, 1))
+        data = bt.feeds.PandasData(dataname=df)
+        cerebro.adddata(data)
+
+        # Set our desired cash start
+        cerebro.broker.setcash(startcash)
+
+        # Set up pyfolio
+        cerebro.addanalyzer(bt.analyzers.PyFolio, _name='pyfolio')
+
+        # Run over everything
+        results = cerebro.run()
         print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
--- PandasRepo.py
+++ PandasRepo.py
@@ -8,22 +8,28 @@ class PandasRepo:
     def __init__(self):
         self._dataFileFormat = os.path.dirname(os.path.abspath(__file__)) + "/data/{}.dat"
         self._dfEmpty = pd.DataFrame(columns=['Close', 'High', 'Low', 'Open', 'Volume'])
-        self._startDate = datetime(2002,2,2)
         self._localCache = {}
         pass
 
-    def GetData(self, symbol:str):
+    def GetData(self, symbol:str, startDate: datetime, endDate: datetime):
         dfCached = self.GetDataFromCache(symbol)
-        dateToBeginPull = self._startDate
         if not dfCached.empty:
-            dateToBeginPull = dfCached.index[-1][1].to_pydatetime() + timedelta(days=1)
+            return dfCached
 
-        dfNew = self.GetDataFromWeb(symbol, dateToBeginPull)
-        df = dfNew
-        if not dfCached.empty:
-            df = pd.concat([dfCached, dfNew])
-        self.CacheData(df, symbol)
-        return df
+        dfNew = self.GetDataFromWeb(symbol, startDate, endDate)
+        self.CacheData(dfNew, symbol)
+        return dfNew
+
+        #dateToBeginPull = startDate
+        #if not dfCached.empty:
+        #    dateToBeginPull = dfCached.index[-1][1].to_pydatetime() + timedelta(days=1)
+
+        #dfNew = self.GetDataFromWeb(symbol, dateToBeginPull, endDate)
+        #df = dfNew
+        #if not dfCached.empty:
+        #    df = pd.concat([dfCached, dfNew])
+        #self.CacheData(df, symbol)
+        #return df
 
     def GetDataFromCache(self, symbol:str):
         if symbol in self._localCache:
@@ -35,17 +41,19 @@ class PandasRepo:
             self._localCache[symbol] = df
         return df
 
-    def GetDataFromWeb(self, symbol:str, startDate:datetime):
+    def GetDataFromWeb(self, symbol:str, startDate:datetime, endDate:datetime):
         df = self._dfEmpty
         try:
-            df = web.DataReader(symbol, 'morningstar', startDate, datetime.today())
+            df = web.DataReader(symbol, 'morningstar', startDate, endDate)
             df.dropna()
             df['Close'].ffill()
+            df = df.reset_index(level=[0])
         except:
             pass
         return df
 
     def CacheData(self, df, symbol:str):
+        self._localCache[symbol] = df
         filename = self._dataFileFormat.format(symbol)
         df.to_pickle(filename)
         return
