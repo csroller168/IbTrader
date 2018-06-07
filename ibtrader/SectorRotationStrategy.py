be83d7f04fe5670e59c75b1c0fbd1b8bca76790e
csroller168 <chrisshort168@gmail.com>
2018-06-07 19:42:15 -0400

    working backtest

--- ibtrader/SectorRotationStrategy.py
+++ ibtrader/SectorRotationStrategy.py
@@ -19,41 +19,29 @@ class SectorRotationStrategy(bt.Strategy):
             smaSlow = bt.ind.SMA(data, period=slowSmaDays)
             self.momentum[symbol] = smaFast / smaSlow
 
-    def notify_data(self, data, status, *args, **kwargs):
-        print("got notify_data")
-        pass
-        #print('*' * 5, 'DATA NOTIF:', data._getstatusname(status), *args)
-        #if status == data.LIVE:
-        #    self.data_live = True
-
     def notify_order(self, order):
-        print("got notify_order")
         if order.status == order.Completed:
             buysell = 'BUY ' if order.isbuy() else 'SELL'
-            txt = '{} {}@{}'.format(buysell, order.executed.size,
-                                    order.executed.price)
+            txt = '{},{},{},{}@{}'.format(self.data.datetime.date(),
+                                          buysell,
+                                          self.data._dataname['Symbol'][0],
+                                          order.executed.size,
+                                          order.executed.price)
             print(txt)
 
     def notify_timer(self, timer, when, *args, **kwargs):
-        print("got notify_timer")
         momentums = dict()
         for symbol in self.getdatanames():
             momentums[symbol] = self.momentum[symbol][0]
 
         buyThreshold = sorted(momentums.values())[-min(self.maxNumPositions, len(momentums))]
         symbolsToBuy = {k: v for k, v in momentums.items() if v >= buyThreshold}
-        pct = 1.0 / len(symbolsToBuy)
+        pct = 1.0 / len(symbolsToBuy) if len(symbolsToBuy) > 0 else 0.0
 
         for symbol in self.getdatanames():
             if symbol in symbolsToBuy:
                 self.order_target_percent(data=self.getdatabyname(symbol), target=pct)
-                print('Buy ' + str(pct) + '% ' + symbol)
-                # self.buy(data=self.getdatabyname(symbol), size=1)
+                print('{} Buy {}% {}'.format(when, pct, symbol))
             else:
                 self.order_target_percent(data=self.getdatabyname(symbol), target=0)
-                print('Sell ' + symbol)
-
-    def next(self):
-        print("got next")
-        pass
-
+                print('{} Sell 100% {}'.format(when, symbol))
